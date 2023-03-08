09ec8d99c1a4b04ac80cf23dc99fe23e6d15c0c8
Anna Luiza <luizafistarol@gmail.com>
2023-03-07 23:23:11 -0300

    feat: add StepsMenu

--- src/components/Background/index.jsx
+++
@@ -1,7 +0,0 @@
-import style from './style.module.css';
-
-export default function Background() {
-    return (
-        <h1>BACKGROUND</h1>
-    );
-};
---
+++ src/components/StepsMenu/index.jsx
@@ -0,0 +1,43 @@
+import style from './style.module.css';
+import React from 'react';
+
+const StepsMenu = (props) => {
+    const display = props.display;
+    const steps = [
+        {
+            stepNumber: 1,
+            stepText: 'your info'
+        },
+        {
+            stepNumber: 2,
+            stepText: 'select plan'
+        },
+        {
+            stepNumber: 3,
+            stepText: 'add-ons'
+        },
+        {
+            stepNumber: 4,
+            stepText: 'summary'
+        }
+    ];
+
+    return (
+        <div className={style.stepsMenu}>
+            <ul>
+                {steps.map((step) => {
+                    return (
+                        <li key={step.stepNumber}>
+                            <button>{step.stepNumber}</button>
+                            <p>STEP {step.stepNumber}</p>
+                            <h3>{step.stepText}</h3>
+                        </li>
+                    );
+                })}
+            </ul>
+            <img alt='fundo colorido' src={`../../../assets/images/bg-sidebar-${display}.svg`}></img>
+        </div>
+    );
+};
+
+export default StepsMenu;
